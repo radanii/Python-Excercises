26a038b70af3bcfa8331eb88a4d35becb02e884f
Radanii <patrick.o.o@hotmail.de>
2019-07-29 12:16:15 +0200

    Hw13 incl. guard

--- Homework 13/modular_guessing_game/secret_number_game.py
+++ Homework 13/modular_guessing_game/secret_number_game.py
@@ -8,29 +8,31 @@ from secret_number_scores_funct import delete_scores
 
 pw = "radani"
 
-while True:
-    print("\n" + "Welcome to <--**GUESS MY SECRET NUMBER!**-->")
-    menu_choice = str.upper(input("Please choose between: " + "\n" +
-                                  "A) New Game" + "\n" +
-                                  "B) Top 3 Scoreboard" + "\n" +
-                                  "C) View Full Scoreboard" + "\n" +
-                                  "D) Quit Game" + "\n" +
-                                  "E) Delete Scores - (password protected)" + "\n" +
-                                  "In order to select please type in A,B,C,D or E here:"))
-    if menu_choice == "A":
-        play_game()
-    elif menu_choice == "B":
-        get_top3_scores()
-    elif menu_choice == "C":
-        get_full_scores()
-    elif menu_choice == "D":
-        break
-    elif menu_choice == "E":
-        password = input("Please enter the password to continue: ")
-        if password == pw:
-            if __name__ == '__main__':
-                delete_scores()
+if __name__ == '__main__':
+
+    while True:
+        print("\n" + "Welcome to <--**GUESS MY SECRET NUMBER!**-->")
+        menu_choice = str.upper(input("Please choose between: " + "\n" +
+                                      "A) New Game" + "\n" +
+                                      "B) Top 3 Scoreboard" + "\n" +
+                                      "C) View Full Scoreboard" + "\n" +
+                                      "D) Quit Game" + "\n" +
+                                      "E) Delete Scores - (password protected)" + "\n" +
+                                      "In order to select please type in A,B,C,D or E here:"))
+        if menu_choice == "A":
+            play_game()
+        elif menu_choice == "B":
+            get_top3_scores()
+        elif menu_choice == "C":
+            get_full_scores()
+        elif menu_choice == "D":
+            break
+        elif menu_choice == "E":
+            password = input("Please enter the password to continue: ")
+            if password == pw:
+                if __name__ == '__main__':
+                    delete_scores()
+            else:
+                print("Wrong password!")
         else:
-            print("Wrong password!")
-    else:
-        print("\n" + "Please select a VALID menu-option!" + "\n")
+            print("\n" + "Please select a VALID menu-option!" + "\n")
